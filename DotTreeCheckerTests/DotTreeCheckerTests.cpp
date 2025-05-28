6eba95d6a8c428d0f75281303542efeb5d1fec17
Алина <@HRISTOS>
2025-05-28 21:25:48 +0300

    Добавьте файлы проекта.

---
+++ DotTreeCheckerTests/DotTreeCheckerTests.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+//#include "../DotTreeChecker/GraphChecker.h" // Проверь и поправь путь, если надо
+
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace DotTreeCheckerTests
+{
+	TEST_CLASS(SanityCheck)
+	{
+	public:
+		TEST_METHOD(TestAlwaysPasses)
+		{
+			Assert::IsTrue(true);
+		}
+	};
+}
+
