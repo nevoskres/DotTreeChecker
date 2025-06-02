b7fa57cf2fc4b12f8a3d8f49e3f9ffc3ca73f02c
Alina Solomenova <solomenova.lina@yandex.ru>
2025-06-02 15:36:05 +0300

    Обновлен test_DFS.cpp (1): добавлены 6 тестов (одиночная вершина, цикл, компоненты связности и др.)

--- DotTreeCheckerTests/test_DFS.cpp
+++ DotTreeCheckerTests/test_DFS.cpp
@@ -73,5 +73,124 @@ namespace DFSTests
 			Assert::AreEqual(2, adjMatrix[0][1]);
 		}
 
+		TEST_METHOD(DFS_threeVerticesCycle)
+		{
+			vector<vector<int>> adjMatrix = { {0,1,0},{0,0,1},{1,0,0} };
+			vector<bool> visited(3, false);
+			vector<pair<int, int>> way;
+
+			DFS(0, visited, adjMatrix, way);
+
+			Assert::IsTrue(visited[0]&&visited[1]&&visited[2]);
+
+			Assert::AreEqual(size_t(2), way.size());
+			Assert::IsTrue((way[0] == make_pair(0, 1))&&(way[1] == make_pair(1, 2)));
+
+			Assert::AreEqual(2, adjMatrix[0][1]);
+			Assert::AreEqual(2, adjMatrix[1][2]);
+			Assert::AreEqual(1, adjMatrix[2][0]);
+
+		}
+
+		TEST_METHOD(DFS_twoWeaklyConnectedComponents)
+		{
+			vector<vector<int>> adjMatrix = { {0,1,1,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,1},{0,0,0,0,0} };
+			vector<bool> visited(5, false);
+			vector<pair<int, int>> way;
+
+			DFS(0, visited, adjMatrix, way);
+
+			Assert::IsTrue(visited[0]&&visited[1]&&visited[2]&&!visited[3]&&!visited[4]);
+
+			Assert::AreEqual(size_t(2), way.size());
+			Assert::IsTrue((way[0] == make_pair(0, 1)) && (way[1] == make_pair(0, 2)));
+
+			Assert::AreEqual(2, adjMatrix[0][1]);
+			Assert::AreEqual(2, adjMatrix[0][2]);
+			Assert::AreEqual(1, adjMatrix[3][4]);
+		}
+
+		TEST_METHOD(DFS_completeGraphThreeVertices)
+		{
+			vector<vector<int>> adjMatrix = { {0,1,1},{1,0,1},{1,1,0} };
+			vector<bool> visited(3, false);
+			vector<pair<int, int>> way;
+
+			DFS(0, visited, adjMatrix, way);
+
+			Assert::IsTrue(visited[0] && visited[1] && visited[2]);
+
+			Assert::AreEqual(size_t(2), way.size());
+			Assert::IsTrue((way[0] == make_pair(0, 1)) && (way[1] == make_pair(1, 2)));
+
+			vector<vector<int>> correctAdjMatrix = {{0,2,1},{1,0,2},{1,1,0} };
+			Assert::IsTrue(adjMatrix == correctAdjMatrix);
+		}
+
+		TEST_METHOD(DFS_vertexWithoutIncoming_notRoot)
+		{
+			vector<vector<int>> adjMatrix = { {0,1,1,0},{0,0,0,0},{0,0,0,0},{0,0,1,0} };
+			vector<bool> visited(4, false);
+			vector<pair<int, int>> way;
+
+			DFS(0, visited, adjMatrix, way);
+
+			Assert::IsTrue(visited[0] && visited[1] && visited[2]);
+			Assert::IsFalse(visited[3]);
+
+			Assert::AreEqual(size_t(2), way.size());
+			Assert::IsTrue((way[0] == make_pair(0, 1)) && (way[1] == make_pair(0, 2)));
+
+			vector<vector<int>> correctAdjMatrix = {{0,2,2,0},{0,0,0,0},{0,0,0,0},{0,0,1,0}};
+			Assert::IsTrue(adjMatrix == correctAdjMatrix);
+		}
+
+		TEST_METHOD(DFS_linearGraphFourVertices)
+		{
+			vector<vector<int>> adjMatrix = { {0,1,0,0},{0,0,1,0},{0,0,0,1},{0,0,0,0} };
+			vector<bool> visited(4, false);
+			vector<pair<int, int>> way;
+
+			DFS(0, visited, adjMatrix, way);
+
+			Assert::IsTrue(visited[0] && visited[1] && visited[2]&& visited[3]);
+
+			Assert::AreEqual(size_t(3), way.size());
+			Assert::IsTrue((way[0] == make_pair(0, 1)) && (way[1] == make_pair(1, 2))&&(way[2] == make_pair(2, 3)));
+
+			vector<vector<int>> correctAdjMatrix = { {0,2,0,0},{0,0,2,0},{0,0,0,2},{0,0,0,0} };
+			Assert::IsTrue(adjMatrix == correctAdjMatrix);
+		}
+
+		TEST_METHOD(DFS_treeElevenVertices)
+		{
+			vector<vector<int>> adjMatrix = { {0,1,0,0,1,1,0,0,0,0,0},
+				{0,0,1,0,0,0,0,0,0,0,0},{0,0,0,1,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,1,1,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0,1},{0,0,0,0,0,0,0,0,1,0,0},
+				{0,0,0,0,0,0,0,0,0,1,0},{0,0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0} };
+			vector<bool> visited(11, false);
+			vector<pair<int, int>> way;
+
+			DFS(0, visited, adjMatrix, way);
+
+			for (bool v : visited) Assert::IsTrue(v);
+
+			Assert::AreEqual(size_t(10), way.size());
+			Assert::IsTrue((way[0] == make_pair(0, 1)) && (way[1] == make_pair(1, 2)) && (way[2] == make_pair(2, 3))
+				&&(way[3] == make_pair(0, 4))&&(way[4] == make_pair(4, 6))&&(way[5] == make_pair(6, 8))
+				&&(way[6] == make_pair(4, 7))&&(way[7] == make_pair(7, 9))&&(way[8] == make_pair(0, 5))
+				&&(way[9] == make_pair(5, 10)));
+
+			vector<vector<int>> correctAdjMatrix = { {0,2,0,0,2,2,0,0,0,0,0},
+				{0,0,2,0,0,0,0,0,0,0,0},{0,0,0,2,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,2,2,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0,2},{0,0,0,0,0,0,0,0,2,0,0},
+				{0,0,0,0,0,0,0,0,0,2,0},{0,0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0} };
+			Assert::IsTrue(adjMatrix == correctAdjMatrix);
+		}
+
 	};
 }
