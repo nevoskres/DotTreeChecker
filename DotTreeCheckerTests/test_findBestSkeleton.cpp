37ea3d315ae92516fcdb9f9825fa7c82c019de0a
Alina Solomenova <solomenova.lina@yandex.ru>
2025-06-04 16:21:46 +0300

    Добавлено Doxygen - описание файла test_findBestSkeleton.cpp и комментарии к тестовым методам

--- DotTreeCheckerTests/test_findBestSkeleton.cpp
+++ DotTreeCheckerTests/test_findBestSkeleton.cpp
@@ -5,6 +5,10 @@
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
 
+/**
+ * @file test_findBestSkeleton.cpp
+ * @brief Набор модульных тестов для функции findBestSkeleton из graph_utils.h
+ */
 
 namespace findBestSkeletonTests
 {
@@ -12,6 +16,7 @@ namespace findBestSkeletonTests
 	{
 	public:
 
+		/// @brief Тест findBestSkeleton: граф из одной вершины
 		TEST_METHOD(findBestSkeleton_singleVertex)
 		{
 			vector<vector<int>> adjMatrix = { {0} };
@@ -20,6 +25,7 @@ namespace findBestSkeletonTests
 			Assert::IsTrue(skeleton[0] == make_pair(0, 0));
 		}
 
+		/// @brief Тест findBestSkeleton: две изолированные вершины
 		TEST_METHOD(findBestSkeleton_twoDisconnectedVertices)
 		{
 			vector<vector<int>> adjMatrix = {{0,0},{0,0}};
@@ -28,6 +34,7 @@ namespace findBestSkeletonTests
 			Assert::IsTrue(skeleton[0] == make_pair(0, 0));
 		}
 
+		/// @brief Тест findBestSkeleton: две соединённые вершины
 		TEST_METHOD(findBestSkeleton_twoConnectedVertices)
 		{
 			vector<vector<int>> adjMatrix = {{0,1},{0,0}};
@@ -36,6 +43,7 @@ namespace findBestSkeletonTests
 			Assert::IsTrue(skeleton[0] == make_pair(0, 1));
 		}
 
+		/// @brief Тест findBestSkeleton: три соединённые вершины и одна изолированная
 		TEST_METHOD(findBestSkeleton_twoConnectedVertices_oneIsolated)
 		{
 			vector<vector<int>> adjMatrix = {{0,1,0,0},{0,0,1,0},{0,0,0,0},{0,0,0,0}};
@@ -45,6 +53,7 @@ namespace findBestSkeletonTests
 			Assert::IsTrue(skeleton[1] == make_pair(1, 2));
 		}
 
+		/// @brief Тест findBestSkeleton: две вершины без входящих дуг
 		TEST_METHOD(findBestSkeleton_twoVerticesWithoutIncoming)
 		{
 			vector<vector<int>> adjMatrix = {{0,1,0},{0,0,0},{0,1,0}};
@@ -53,6 +62,7 @@ namespace findBestSkeletonTests
 			Assert::IsTrue(skeleton[0] == make_pair(0, 1));
 		}
 
+		/// @brief Тест findBestSkeleton: две вершины без входящих дуг (с разной достижимостью из них)
 		TEST_METHOD(findBestSkeleton_unequalSizeTwoVerticesWithoutIncoming)
 		{
 			vector<vector<int>> adjMatrix = { {0,1,0,0},{0,0,0,0},{0,1,0,1},{0,0,0,0} };
@@ -62,6 +72,7 @@ namespace findBestSkeletonTests
 			Assert::IsTrue(skeleton[1] == make_pair(2, 3));
 		}
 
+		/// @brief Тест findBestSkeleton: линейный граф из четырёх вершин
 		TEST_METHOD(findBestSkeleton_linearGraphFourVertices)
 		{
 			vector<vector<int>> adjMatrix = {{0,1,0,0},{0,0,1,0},{0,0,0,1},{0,0,0,0}};
@@ -72,6 +83,7 @@ namespace findBestSkeletonTests
 			Assert::IsTrue(skeleton[2] == make_pair(2, 3));
 		}
 
+		/// @brief Тест findBestSkeleton: цикл из трёх вершин
 		TEST_METHOD(findBestSkeleton_cycleGraph)
 		{
 			vector<vector<int>> adjMatrix = {{0,1,0},{0,0,1},{1,0,0}};
@@ -81,6 +93,7 @@ namespace findBestSkeletonTests
 			Assert::IsTrue(skeleton[1] == make_pair(1, 2));
 		}
 
+		/// @brief Тест findBestSkeleton: две компоненты одинакового размера и две изолированные вершины
 		TEST_METHOD(findBestSkeleton_equalSizeComponents)
 		{
 			vector<vector<int>> adjMatrix = {
@@ -96,6 +109,7 @@ namespace findBestSkeletonTests
 			Assert::IsTrue(skeleton[0] == make_pair(0, 1));
 		}
 
+		/// @brief Тест findBestSkeleton: две компоненты разного размера
 		TEST_METHOD(findBestSkeleton_disconnectedComponents)
 		{
 			vector<vector<int>> adjMatrix = {
@@ -110,6 +124,7 @@ namespace findBestSkeletonTests
 			Assert::IsTrue(skeleton[0] == make_pair(0, 1));
 		}
 
+		/// @brief Тест findBestSkeleton: полный граф из трёх вершин
 		TEST_METHOD(findBestSkeleton_completeGraphThreeVertices)
 		{
 			vector<vector<int>> adjMatrix = {{0,1,1},{1,0,1},{1,1,0}};
@@ -119,6 +134,7 @@ namespace findBestSkeletonTests
 			Assert::IsTrue(skeleton[1] == make_pair(1, 2));
 		}
 
+		/// @brief Тест findBestSkeleton: дерево из 11 вершин
 		TEST_METHOD(findBestSkeleton_treeElevenVertices)
 		{
 			vector<vector<int>> adjMatrix = { {0,0,0,0,0,0,1,1,0,0,0},
@@ -138,3 +154,4 @@ namespace findBestSkeletonTests
 
 	};
 }
+
