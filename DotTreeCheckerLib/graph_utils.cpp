f29103ca7dcb87337024efc34dee26e1306948ad
Alina Solomenova <solomenova.lina@yandex.ru>
2025-06-09 20:14:43 +0300

    Обновлен graph_utils.cpp (2): добавлена реализация функций trim и parseDotFile

--- DotTreeCheckerLib/graph_utils.cpp
+++ DotTreeCheckerLib/graph_utils.cpp
@@ -67,3 +67,81 @@ vector<pair<int, int>> findBestSkeleton(vector<vector<int>> matrixCopy)
 	//Вернуть лучший остов (путь)
 	return skeleton;
 }
+
+
+Graph parseDotFile(const vector<string>& lines)
+{
+
+	Graph g;
+	g.setDotStringGraph(lines);
+
+	map<int, int> vertexToIndex;        // отображение: имя вершины индекс
+	vector<int> indexToVertex;          // обратное отображение: индекс имя вершины
+	vector<pair<int, int>> edges;       // список дуг
+
+	// Обрабатываем строки
+	for (const string& line : lines)
+	{
+		if (line == "digraph G" || line == "{" || line == "}") continue;
+
+		size_t arrowPos = line.find("->");
+		if (arrowPos != string::npos)
+		{
+			// Дуга: a->b
+			int from = stoi(line.substr(0, arrowPos));
+			int to = stoi(line.substr(arrowPos + 2));
+
+			edges.emplace_back(from, to);
+
+			// Регистрируем вершины
+			if (vertexToIndex.find(from) == vertexToIndex.end()) {
+				vertexToIndex[from] = indexToVertex.size();
+				indexToVertex.push_back(from);
+			}
+			if (vertexToIndex.find(to) == vertexToIndex.end()) {
+				vertexToIndex[to] = indexToVertex.size();
+				indexToVertex.push_back(to);
+			}
+		}
+		else
+		{
+			// Изолированная вершина
+			int v = stoi(line);
+			if (vertexToIndex.find(v) == vertexToIndex.end()) {
+				vertexToIndex[v] = indexToVertex.size();
+				indexToVertex.push_back(v);
+			}
+		}
+	}
+
+	// Создание матрицы смежности
+	int n = indexToVertex.size();
+	vector<vector<int>> adjacencyMatrix(n, vector<int>(n, 0));
+
+	for (const auto& edge : edges)
+	{
+		int i = vertexToIndex[edge.first];
+		int j = vertexToIndex[edge.second];
+		adjacencyMatrix[i][j] = 1;
+	}
+
+	g.setVertexToIndex(vertexToIndex);
+	g.setIndexToVertex(indexToVertex);
+	g.setAdjacencyMatrix(adjacencyMatrix);
+
+	return g;
+}
+
+
+string trim(const string& s) 
+{
+	size_t start = 0;
+	while (start < s.size() && isspace(static_cast<unsigned char>(s[start]))) {
+		++start;
+	}
+	size_t end = s.size();
+	while (end > start && isspace(static_cast<unsigned char>(s[end - 1]))) {
+		--end;
+	}
+	return s.substr(start, end - start);
+}
