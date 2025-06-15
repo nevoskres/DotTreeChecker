2391d29ce659cb8a9f93c01eaa77e56876cb225d
Alina Solomenova <solomenova.lina@yandex.ru>
2025-06-15 20:28:33 +0300

    Добавлен DotTreeCheckerLib.h — мастер-заголовок для всей библиотеки DotTreeCheckerLib

---
+++ DotTreeCheckerLib/DotTreeCheckerlib.h
@@ -0,0 +1,14 @@
+/**
+ * @file DotTreeCheckerlib.h
+ * @brief Главный заголовочный файл библиотеки, включающий все необходимые интерфейсы.
+ *
+ * @details Этот файл служит единым входным пунктом для подключения библиотеки.
+ */
+
+#pragma once
+
+#include"Export.h"
+#include"Error.h"
+#include"File.h"
+#include"Graph.h"
+#include"graph_utils.h"
