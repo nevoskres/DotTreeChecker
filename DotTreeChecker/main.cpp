2d4c1d09b2e2b8235dc6478f5ae4a79593eae54b
Alina Solomenova <solomenova.lina@yandex.ru>
2025-06-15 23:03:42 +0300

    Обновлен main.cpp (3): написан код программы

--- DotTreeChecker/main.cpp
+++ DotTreeChecker/main.cpp
@@ -1,19 +1,71 @@
+#include <locale> 
+#include <windows.h> 
+#include <iostream>
+#include <fstream>
+
+#include "../DotTreeCheckerlib/DotTreeCheckerlib.h"
+
+using namespace std;
 
 int main(int argc, char* argv[])
 {
-	//Считать из параметров командной строки путь ко входному файлу
-	//Считать из параметров командной строки путь для выходного файла
-	//Если удалось открыть входной файл и ошибок не обнаружено 
-		//Считать данные из файла 
-	//Иначе, если удалось открыть файл и ошибки были обнаружены
-		//Вернуть все обнаруженные ошибки 
-	//Иначе 
-		//Вернуть ошибку о неудочном открытии файла
-	//Закрыть файл
-	//Если файл считан
-		//Выполнить проверку - является ли граф деревом
-		//Занести результат проверки в выходной файл
+	SetConsoleOutputCP(CP_UTF8);
+	SetConsoleCP(CP_UTF8);
+	setlocale(LC_ALL, "ru_RU.UTF-8");
 
-	//Завершить работу программы (вернуть параметр показывающий результат проверки)
+	//Считать из параметров командной строки путь ко входному файлу
+	auto filePaths = extractFilePaths(argc,argv);
+	string inputFP = filePaths.first;
+	//Считать из параметров командной строки путь для выходного файла
+	string outputFP = filePaths.second;
+
+	
+	if (inputFP.empty())
+	{
+		cerr << "Не указан входной файл\n";
+		return 1;
+	}
+	else if (outputFP.empty())
+	{
+		cerr << "Не указан выходной файл\n";
+		return 1;
+	}
+
+	bool errorOpenInputFile = false;
+	auto lines = readFile(inputFP, errorOpenInputFile);
+
+	if (errorOpenInputFile) return 1;
+
+	Error errorOb;
+
+	errorOb.findErrors(lines);
+	
+	if (!errorOb.getErrors().empty())
+	{
+		const auto& errors = errorOb.getErrors();
+		for (const auto& err : errors)
+		{
+			std::string msg = err.generateErrorMessage(err.getType());
+			std::cerr << msg << std::endl;
+		}
+		return 1;  
+	}
+	
+	Graph g = parseDotFile(cleanLines(lines));
+
+	vector<pair<int, int>> bestSkeleton = findBestSkeleton(g.getAdjacencyMatrix());
+
+	outMessForGraph message;
+
+	vector<string> checkedDotGraph = writeDotFile(g.getAdjacencyMatrix(),g.getIndexToVertex(),
+		bestSkeleton,g.getDotStringGraph(),message);
+		
+	bool openOutputFile = writeLinesToFile(checkedDotGraph, outputFP);
+			
+	if (!openOutputFile)
+	{
+		return 1;
+	}
+
 	return 0;
 }
