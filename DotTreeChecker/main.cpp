bd4d3b2092f2d58f2700690d43be7e5b1a05aecd
Alina Solomenova <solomenova.lina@yandex.ru>
2025-06-16 00:15:12 +0300

    Обновлен main.cpp (4): исправлен вывод ошибок с поддержкой русских символов через std::wcerr

--- DotTreeChecker/main.cpp
+++ DotTreeChecker/main.cpp
@@ -2,6 +2,8 @@
 #include <windows.h> 
 #include <iostream>
 #include <fstream>
+#include <io.h>
+#include <fcntl.h>
 
 #include "../DotTreeCheckerlib/DotTreeCheckerlib.h"
 
@@ -13,6 +15,12 @@ int main(int argc, char* argv[])
 	SetConsoleCP(CP_UTF8);
 	setlocale(LC_ALL, "ru_RU.UTF-8");
 
+	_setmode(_fileno(stdout), _O_U16TEXT);
+	_setmode(_fileno(stderr), _O_U16TEXT);
+
+	std::wcout << L"выводится wcout" << std::endl;
+	std::wcerr << L"выводится wcerr" << std::endl;
+
 	//Считать из параметров командной строки путь ко входному файлу
 	auto filePaths = extractFilePaths(argc,argv);
 	string inputFP = filePaths.first;
@@ -22,12 +30,12 @@ int main(int argc, char* argv[])
 	
 	if (inputFP.empty())
 	{
-		cerr << "Не указан входной файл\n";
+		wcerr << "Не указан входной файл\n";
 		return 1;
 	}
 	else if (outputFP.empty())
 	{
-		cerr << "Не указан выходной файл\n";
+		wcerr << "Не указан выходной файл\n";
 		return 1;
 	}
 
@@ -44,7 +52,7 @@ int main(int argc, char* argv[])
 	{
 		const auto& errors = errorOb.getErrors();
 		for (const auto& err : errors)
-			std::cerr << err.generateErrorMessage() << std::endl;
+			wcerr << utf8ToUtf16(err.generateErrorMessage() ) << endl;
 		return 1;  
 	}
 	
@@ -65,4 +73,13 @@ int main(int argc, char* argv[])
 	}
 
 	return 0;
+}
+
+std::wstring utf8ToUtf16(const std::string& utf8)
+{
+	if (utf8.empty()) return L"";
+	int sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, utf8.data(), (int)utf8.size(), nullptr, 0);
+	std::wstring wstrTo(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, utf8.data(), (int)utf8.size(), &wstrTo[0], sizeNeeded);
+	return wstrTo;
 }
